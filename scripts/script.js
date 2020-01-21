0ffa71a8b8480889e9c0346a0ddd55ab4218ddb7
SoLazy42 <nikitadenisovgood@mail.ru>
2020-01-21 20:58:12 +0700

    add style file and update script file

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,23 @@ let app = new Vue({
     el: '#app',
 
     data: {
+        seen: false,
         list: '',
         lists: [],
     },
     methods: {
         addList: function() {
-            this.lists.push(this.list)
-            this.list = ''
+            if (this.list !== '') {
+                this.lists.push(this.list)
+                this.list = ''
+                this.seen = true
+            }
         },
         removeList: function() {
             this.lists.pop()
-        }
+            this.lists.length === 0 ? this.seen = false : this.seen = true
+        },
+        
     }
 
 })
